91b4b09762398b0207334ce667e5124ce9dcef3a
Roberto Hernandez <rh@fusion52.com>
2019-02-09 12:00:18 -0800

    Intro to Lifecycle Methods, Refactoring Data Loading to Lifecycle Methods, and Alternate State Initialization

--- prod/seasons/src/index.js
+++ prod/seasons/src/index.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
+// import SeasonDisplay from './SeasonDisplay';
 
 // Functional component
 // const App = () => {
 // };
 
 class App extends React.Component {
-  constructor(props) {
-    // reference to parent constructor
-    super(props);
+  // // NOT RECOMMENDED to data load here!
+  // constructor(props) {
+  //   // reference to parent constructor
+  //   super(props);
 
-    // initialize state, only time of direct assignment
-    this.state = { lat: null, errorMessage: '' };
+  //   // // initialize state, only time of direct assignment
+  //   // this.state = { lat: null, errorMessage: '' };
+  // }
+
+  // alternate way to initialize!
+  state = { lat: null, errorMessage: '' };
+
+  // // Data load here!
+  // // Good place to do data-loading!
+  // componentDidMount() {
+  //   console.log('My component was rendered to the screen');
+  // }
+  //
+  // // Good place to do more data-loading when state/props change
+  // componentDidUpdate() {
+  //   console.log('My component was just updated - it rerendered!');
+  // }
+  //
+  // // Good place to do cleanup (especially for non-React stuff)
+  // componentWillUnmount(){
+  //   console.log('My component was just closed!');
+  // }
+
+  componentDidMount() {
+    console.log('My component was rendered to the screen');
 
     // may take 3-4 seconds to get location.
     window.navigator.geolocation.getCurrentPosition(
