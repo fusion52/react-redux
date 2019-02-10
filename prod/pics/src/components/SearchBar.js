ec22da4914a45dd9509e5f664ef66654b05a43eb
Roberto Hernandez <rh@fusion52.com>
2019-02-09 23:01:00 -0800

    Handling user input with Forms and events

--- prod/pics/src/components/SearchBar.js
+++ prod/pics/src/components/SearchBar.js
@@ -5,17 +5,31 @@ import React from 'react';
 // };
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
+
+  state = { term: '' };
+
+  // preferred way to handle
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    // console.log(this.state.term);
+    this.props.onSubmit(this.state.term);
+  };
 
   render() {
     return (
       <div className='ui segment'>
-        <form className='ui form'>
+        <form onSubmit={this.onFormSubmit} className='ui form'>
           <div className='field'>
             <label>Image Search</label>
-            <input type='text' onChange={this.onInputChange} />
+            <input
+              type='text'
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
