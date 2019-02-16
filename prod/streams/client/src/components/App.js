3409c494d7dfbe81ca626a1b0fcd57b7166703b3
Roberto Hernandez <rh@fusion52.com>
2019-02-15 17:30:34 -0800

    v234-267.  REST-Based React Apps.  Creating streams, REST-ful conventions, API server, creating streams through acction creators, REUSE code create and edit, PUT vs PATCH

--- prod/streams/client/src/components/App.js
+++ prod/streams/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
@@ -7,6 +7,7 @@ import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import Header from './Header';
+import history from '../history';
 
 // const PageOne = () => {
 //   return (
@@ -30,18 +31,18 @@ import Header from './Header';
 const App = () => {
   return (
     <div className='ui container'>
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header />
           {/* <Route path='/' exact component={PageOne} /> */}
           {/* <Route path='/pagetwo' component={PageTwo} /> */}
           <Route path='/' exact component={StreamList} />
           <Route path='/streams/new' exact component={StreamCreate} />
-          <Route path='/streams/edit' exact component={StreamEdit} />
+          <Route path='/streams/edit/:id' exact component={StreamEdit} />
           <Route path='/streams/delete' exact component={StreamDelete} />
           <Route path='/streams/show' exact component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
